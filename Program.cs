fc8eeb0e649c8325acd8c5a6e9e5b9c0d26610ac
Zackery Ayscue <ztayscue@Zackerys-MacBook-Pro.local>
2018-08-21 14:39:41 -0400

    Adding the first sample project for learning the ML.Net library.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace ML.Net.Samples
+{
+    class Program
+    {
+        public class IrisData
+        {
+            [Column("0")]
+            public float SepalLength;
+            
+            [Column("1")]
+            public float SepalWidth;
+
+            [Column("2")]
+            public float PetalLength;
+
+            [Column("3")]
+            public float PetalWidth;
+
+            [Column("4")]
+            [ColumnName("Label")]
+            public string Label;
+        }
+
+        public class IrisPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+
+        static void Main(string[] args)
+        {
+            var pipeline = new LearningPipeline();
+            string dataPath = "./iris.data.txt";
+            pipeline.Add(new TextLoader(dataPath).CreateFrom<IrisData>(separator: ','));
+            pipeline.Add(new Dictionarizer("Label"));
+            pipeline.Add(new ColumnConcatenator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+            var model = pipeline.Train<IrisData, IrisPrediction>();
+            var prediction = model.Predict(new IrisData()
+            {
+                SepalLength = 3.3f,
+                SepalWidth = 1.6f,
+                PetalLength = 0.2f,
+                PetalWidth = 5.1f,
+            });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+        }
+    }
+}
